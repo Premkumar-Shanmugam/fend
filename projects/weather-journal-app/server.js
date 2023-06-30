9ffd91180e0ab3955fc80bdd36bd0540c98558a8
Premkumar Shanmugam <premkumar.shanmugam@gmail.com>
2023-06-30 20:56:00 +0530

    Add weather app changes

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -2,18 +2,51 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors')
+app.use(cors())
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+app.get('/data', getData);
+
+app.post('/data', addData);
+
+function getData(req, res) {
+    res.send(projectData);
+}
+
+function addData(req, res) {
+    reqBody = req.body;
+
+    projectData = {
+        temperature: reqBody.temperature,
+        date: reqBody.date,
+        userResponse: reqBody.userResponse
+    }
+    
+    const response = {
+        message: 'Weather data added successfully!'
+    }
+    res.status = 200;
+    res.send(response);
+}
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`app listening on port ${port}`)
+})
--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -1,5 +1,85 @@
 /* Global Variables */
+baseURL = 'https://api.openweathermap.org/data/2.5/weather';
+apiKey = 'c496e137eb92d8a7628c13d61af587c0&units=imperial';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// function to fetch weather data
+const getWeather = async (baseUrl='', appid='', zip='') => {
+
+    const url = baseUrl + '?zip=' + zip + '&appid=' + appid
+
+    const response = await fetch(url);
+
+    try {
+        const weatherData = await response.json();
+        return weatherData;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// function to post weather data
+const postWeather = async (url='', data={}) => {
+
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    });
+
+    try {
+        const newData = await response.json();
+        return newData;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// function to retrieve weather data
+const retrieveWeather = async (url='') => {
+
+    const response = await fetch(url, {
+        method: 'GET',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    try {
+        const projectData = await response.json();
+        return projectData;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+document.getElementById('generate').addEventListener('click', processRequest);
+
+function processRequest() {
+    
+    const zip = document.getElementById('zip').value;
+    
+    getWeather(baseURL, apiKey, zip).then((data) => {    
+        const userResponse = document.getElementById('feelings').value;
+        const weatherData = {
+            temperature: data.main.temp,
+            date: newDate,
+            userResponse: userResponse,
+        }
+        postWeather(url='/data', weatherData).then((data) => {
+            retrieveWeather(url='/data').then((data) => {
+                console.log(data);
+                document.getElementById('date').innerText = data.date;
+                document.getElementById('temp').innerText = Math.round(data.temperature)+ ' degrees'
+                document.getElementById('content').innerText = data.userResponse;
+            });
+        })
+    })
+}
