b68f0597f51d4027a035d6a2474026b4eb579ab4
Premkumar Shanmugam <premkumar.shanmugam@gmail.com>
2023-07-01 05:17:33 +0530

    fix date error

--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -4,7 +4,7 @@ apiKey = 'c496e137eb92d8a7628c13d61af587c0&units=imperial';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth()+1+'.'+ d.getDate()+'.'+ d.getFullYear();
 
 // function to fetch weather data
 const getWeather = async (baseUrl='', appid='', zip='') => {
